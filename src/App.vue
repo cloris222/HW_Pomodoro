3822ba4f465390dd2cc0e6d7f236d55943dd13df
cloris222 <cloris222@gmail.com>
2023-01-08 18:54:35 +0800

    updaterouters

--- src/App.vue
+++ src/App.vue
@@ -5,34 +5,35 @@ v-app
       v-list
         v-list-item
           v-list-item-content
-              v-list-item-title.text-h6 番茄鐘
+              v-list-item-title.text-h6
+                router-link(to="/").router-link 番茄鐘
       v-divider
       v-list
           v-list-item
             v-row.d-flex.justify-center.align-center
-              v-col
-                v-btn(icon="mdi-home" variant="text" to="/" )
-              v-col.col-text.font-weight-bold
-                span 回到首頁
+                  v-col
+                    v-btn(icon="mdi-home" variant="text" to="/")
+                  v-col.col-text.font-weight-bold
+                    router-link(to="/").router-link 回到首頁
           v-list-item
             v-row.d-flex.justify-center.align-center
                 v-col
                   v-btn(icon="mdi-format-list-bulleted" variant="text" to="/list")
                 v-col.col-text.font-weight-bold
-                  span 待辦清單
+                  router-link(to="/list").router-link 待辦清單
           v-list-item
             v-row.d-flex.justify-center.align-center
                   v-col
                     v-btn(icon="mdi-cog" variant="text" to="/settings")
                   v-col.col-text.font-weight-bold
-                    span 設定
+                    router-link(to="/settings").router-link 設定
           v-list-item
             v-row.d-flex.justify-center.align-center
                     v-col
                       v-btn(:icon="notify ? 'mdi-bell' : 'mdi-bell-off'" variant="text" @click="toggleNotify")
                     v-col.col-text.font-weight-bold
-                      span(v-if="notify") 關閉通知
-                      span(v-else) 開啟通知
+                      span(v-if="notify" @click="toggleNotify") 關閉通知
+                      span(v-else @click="toggleNotify") 開啟通知
 
   //- v-app-bar
   //-   v-app-bar-title 番茄鐘
@@ -52,24 +53,23 @@ v-app
 </template>
 
 <script setup>
-// import { computed, ref, onMounted } from 'vue'
+import { computed, ref, onMounted } from 'vue'
 import { useSettingsStore } from '@/stores/settings'
 import { storeToRefs } from 'pinia'
 const settings = useSettingsStore()
 const { notify } = storeToRefs(settings)
 const { toggleNotify } = settings
 
-// const drawer = ref(null)
-// let drawerwidth = 0
-// const updateDrawerWidth = () => {
-//   drawerwidth = drawer.value.offsetWidth
-// }
-// const drawerchange = computed(() => {
-//   return drawerwidth === 70
-// })
+// await nextTick()
+const drawer = ref(null)
+let drawerwidth = 0
 
-// console.log(drawerchange)
+const updateDrawerWidth = computed(() => {
+  drawerwidth = drawer.value.offsetwidth
+  console.log(drawerwidth)
+  return drawerwidth
+})
 
-// onMounted(updateDrawerWidth)
+onMounted(updateDrawerWidth)
 
 </script>
